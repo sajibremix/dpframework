fdb0383c157350243b8e26dc5e5318b5c0b5533b
sajibremix <jshossen01@gmail.com>
2017-11-15 12:41:27 +0600

    ajax function add

---
+++ files/js/javascript.js
@@ -0,0 +1,44 @@
+//Standard Javascript function for making a http get request with callback
+function http_get_request( url, callback, params ) {
+	
+	if (window.XMLHttpRequest)
+	{// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			//callback with response
+			window[callback](xmlhttp.responseText, params);
+		}
+	}
+	xmlhttp.open("GET",url,true);
+	xmlhttp.send();
+}
+
+function http_post_request( url, data, callback, params ) {
+	if (window.XMLHttpRequest)
+	{// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			//callback with response
+			window[callback](xmlhttp.responseText, params);
+		}
+	}
+	xmlhttp.open("POST",url,true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlhttp.send(data);
+}
